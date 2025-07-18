3617f20e2f05bcd38b813ef898c36ca27803c263
09152.chaol.liu <chaol.liu@yitmh.com>
2025-07-18 19:11:43 +0800

    feat: add vant

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "vant/lib/index.css";
+import AppFooter from "~/components/AppFooter.vue";
+</script>
 
 <template>
   <main class="flex flex-col min-h-svh">
-    <AppHeader class="h-[var(--van-nav-bar-height)]" />
-    <div class="p-16 pb-[var(--van-nav-bar-height)] flex-1">
-      <slot />
-    </div>
+    <slot />
     <AppFooter />
   </main>
 </template>
 
 <style lang="css">
-:root {
-  --color-primary: hsl(219, 50%, 10%);
-  --color-secondary: hsl(219, 50%, 90%);
-  --color-tertiary: hsl(279, 80%, 80%);
-  --color-accent: hsl(159, 80%, 80%);
-}
 body {
   font-family: "Noto Sans TC", sans-serif;
   font-optical-sizing: auto;
-  color: var(--color-primary);
   font-style: normal;
 }
 </style>
